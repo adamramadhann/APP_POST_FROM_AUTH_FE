e7d67e56d98d17850a417e357251321943dc427f
adam/modal <adam@adams-MacBook-Pro.local>
2024-10-29 22:11:49 +0700

    delete

--- src/page/Layout/ScrenMyPost.jsx
+++ src/page/Layout/ScrenMyPost.jsx
@@ -6,10 +6,11 @@ import { CgProfile } from 'react-icons/cg'
 import { FaHome } from 'react-icons/fa'
 import { MdPostAdd } from 'react-icons/md'
 import axios from 'axios'
+import adm from '../../axiosApiAuth'
 
 const ScrenMyPost = () => {
     const [tab, setTab] = useState("Home")
-    const [openModal, SetOpenModal] = useState(false)
+    const [openModal, SetOpenModal] = useState(null)
 
     const buttonTab = [
         { id: "1", title: "Home", icon : <FaHome size={30} /> },
@@ -26,11 +27,33 @@ const ScrenMyPost = () => {
                 return dataApi.data
             } catch (error) {
                 console.error(error)
-                return []
             }
             
         }   
     })
+ 
+    const handleOpenModal = (e) => {
+        if (openModal === e) {
+            SetOpenModal(null)
+        } else {
+            SetOpenModal(e)
+        }
+    }
+
+    const handleDelete = async (id) => {
+        const conf = window.confirm("Yakin hapus data ini ??")
+        if(!conf) return 
+
+        try {
+            const deletePost = await adm.delete(`http://localhost:7777/api/post/delete/${id}`)
+            console.info(deletePost)  
+            refetch()  
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
 
     console.info(data)
 
@@ -44,19 +67,19 @@ const ScrenMyPost = () => {
                         <div className='flex flex-col gap-10 h-[760px] w-full overflow-y-auto items-center justify-center ' >
                         {
                             data?.apiGetAll?.map((e) => (
-                                <div key={e.id} className='w-[90%] px-3 bg-white gap-5 flex flex-col relative shadow-lg h-auto pb-6 rounded-md ' >
+                                <div key={e.id} className='w-[90%] px-3 bg-white mt-10 gap-5 flex flex-col relative shadow-lg h-auto pb-6 rounded-md ' >
                                     <h1 className='mt-6 text-xl font-bold text-gray-600' >{e.judul}</h1>
                                     <h1 className='text-gray-500' >{e.description} </h1>
                                     <span className='flex w-full mt-4 text-gray-500  justify-between' >
                                         <h1>{e.author}</h1>
                                         <h1>{new Date(e.createAt).toLocaleDateString()}</h1>
                                     </span>
-                                <AiOutlineBars onClick={() => SetOpenModal(prev => !prev)}  className={`absolute top-3 right-2 ${openModal ? "hidden" : "block"} `} />
-                                <span onClick={() => SetOpenModal(prev = !prev)} className={`flex flex-col absolute top-3 right-4 bg-white ${openModal ? "block" : 'hidden' } `} >
+                                <AiOutlineBars onClick={() => handleOpenModal(e.id)}  className={`absolute top-3 right-2 ${openModal === e.id ? "hidden" : "block"} `} />
+                                <span onClick={() => SetOpenModal(prev = !prev)} className={`flex flex-col absolute top-3 right-4 bg-white ${openModal === e.id? "block" : 'hidden' } `} >
                                     <button onClick={() => SetOpenModal(prev => !prev)} className='text-blue-500' >Edit</button>
-                                    <button onClick={() => SetOpenModal(prev => !prev)} className='text-red-500' >Delete</button>
+                                    <button onClick={() => handleDelete(e.id)} className='text-red-500' >Delete</button>
                                 </span>
-                                    <button onClick={() => SetOpenModal(prev => !prev)} className={`absolute top-0 right-1  text-2xl w-2 h-2 text-red-500 ${openModal ? "block" : "hidden"}`} >x</button>
+                                    <button onClick={() => SetOpenModal(prev => !prev)} className={`absolute top-0 right-1  text-2xl w-2 h-2 text-red-500 ${openModal === e.id ? "block" : "hidden"}`} >x</button>
                                 </div>
                             ))
                         }
@@ -85,8 +108,7 @@ const ScrenMyPost = () => {
                     <button onClick={() => setTab(e.title)} key={e.id} className='flex text-white flex-col items-center justify-center pt-2 gap-2' >
                         {e.icon}
                         { e.title}
-                    </button>
-  <button key={e.id} onClick={() => setTab(e.title)} className={`flex-1 ${buttonTabBar === "Home" && buttonTabBar === "Home" ? "border-r" : null}`} >{e.title}</button>
+                     </button>
                 ))
             }
         </div>
